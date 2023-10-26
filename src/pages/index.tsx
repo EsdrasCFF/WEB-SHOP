424db441ff778369e810e7461488536898ecce9c
EsdrasCFF <ecff.dev@gmail.com>
2023-10-26 11:22:06 -0300

    Style: Visual structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import Image from 'next/image'
 import { styled } from '../styles'
+import { HomeContainer, Product } from '../styles/pages/home'
+
+import image1 from '../assets/shirts/1.png';
+import image2 from '../assets/shirts/2.png';
+import image3 from '../assets/shirts/3.png';
 
 const Button = styled('button', {
   background: '$green300',
@@ -10,8 +16,25 @@ const Button = styled('button', {
 
 export default function Home() {
   return (
-    <Button>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={image1}  width={520} height={520} alt=''/>
+
+        <footer>
+          <strong> Casmisa XYZ </strong>
+          <span> R$ 79,90 </span>
+        </footer>
+      </Product>
+      
+      <Product>
+        <Image src={image2}  width={520} height={520} alt=''/>
+
+        <footer>
+          <strong> Casmisa XYZ </strong>
+          <span> R$ 79,90 </span>
+        </footer>
+      </Product>
+      
+    </HomeContainer>
   )
 }
