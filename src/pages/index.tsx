7f711fb32a8e38b46cdeffb3309ada8d5f861a99
EsdrasCFF <ecff.dev@gmail.com>
2023-10-26 14:39:11 -0300

    Style: Create Carousel at Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { styled } from '../styles'
 import { HomeContainer, Product } from '../styles/pages/home'
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import image1 from '../assets/shirts/1.png';
 import image2 from '../assets/shirts/2.png';
 import image3 from '../assets/shirts/3.png';
@@ -15,9 +17,16 @@ const Button = styled('button', {
 })
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider' >
+      <Product className='keen-slider__slide' >
         <Image src={image1}  width={520} height={520} alt=''/>
 
         <footer>
@@ -26,8 +35,26 @@ export default function Home() {
         </footer>
       </Product>
       
-      <Product>
+      <Product className='keen-slider__slide' >
         <Image src={image2}  width={520} height={520} alt=''/>
+
+        <footer>
+          <strong> Casmisa XYZ </strong>
+          <span> R$ 79,90 </span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide' >
+        <Image src={image3}  width={520} height={520} alt=''/>
+
+        <footer>
+          <strong> Casmisa XYZ </strong>
+          <span> R$ 79,90 </span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide' >
+        <Image src={image3}  width={520} height={520} alt=''/>
 
         <footer>
           <strong> Casmisa XYZ </strong>
